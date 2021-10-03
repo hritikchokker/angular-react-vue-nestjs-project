280910d459b2daae9cdd8936767c1944b1d214f4
hritikchokker <hchokker7@gmail.com>
2021-10-03 13:20:58 +0530

    initial commit

---
+++ apps/client-vue/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'client-vue',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.vue$': '@vue/vue2-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/client-vue',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      babelConfig: '<rootDir>/babel.config.js',
+    },
+    'vue-jest': {
+      tsConfig: 'apps/client-vue/tsconfig.spec.json',
+      babelConfig: 'apps/client-vue/babel.config.js',
+    },
+  },
+};
